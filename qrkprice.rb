dfc065d3b1be98aa4bbba4add808a9a9a462ccb3
Tiem Song <tiem@songfamily.org>
2013-12-12 17:33:57 -0800

    initial commit

---
+++ qrkprice.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'curb'
+
+btcusd_url1 = 'https://btc-e.com/api/2/btc_usd/ticker'
+btcusd_url2 = 'https://coinbase.com/api/v1/currencies/exchange_rates'
+qrkbtc_url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=71'
+
+# fetch BTC/USD data
+btc_html = Curl.get(btcusd_url1).body_str
+btcusd1 = JSON.parse(btc_html)['ticker']['avg'].to_f
+btc_html = Curl.get(btcusd_url2).body_str
+btcusd2 = JSON.parse(btc_html)['btc_to_usd'].to_f
+
+#debug
+puts btcusd1
+puts btcusd2
+btcusd = (btcusd1 + btcusd2) / 2
+
+# fetch QRK/BTC data
+qrk_html = Curl.get(qrkbtc_url).body_str
+qrkbtc = JSON.parse(qrk_html)['return']['markets']['QRK']['lasttradeprice'].to_f
+
+# print outputs
+qrkusd = btcusd * qrkbtc
+puts "1 BTC = #{btcusd} USD"
+puts "1 QRK = #{qrkbtc} BTC"
+puts "1 QRK = #{qrkusd} USD"
